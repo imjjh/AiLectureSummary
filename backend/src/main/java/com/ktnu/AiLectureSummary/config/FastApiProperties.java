6baeec1008072ee879b4f674b41afdc8b594250d
imjjh <rldirwkd328@naver.com>
2025-05-29 21:28:47 +0900

    fix: fastapi 관련 설정 수정

--- backend/src/main/java/com/ktnu/AiLectureSummary/config/FastApiProperties.java
+++ backend/src/main/java/com/ktnu/AiLectureSummary/config/FastApiProperties.java
@@ -6,7 +6,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @Component
-@ConfigurationProperties
+@ConfigurationProperties(prefix = "fastapi")
 @Getter
 @Setter
 public class FastApiProperties {
